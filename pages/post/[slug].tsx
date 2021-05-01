bb4a7053d71f4bbe5ebb041549310bc3b301ab96
subhadip007 <subhadipadhikary007@gmail.com>
2021-05-02 01:56:01 +0530

    changed slug.tsx file to do coment system

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 const BLOG_URL = 'http://localhost:3001'
 const CONTENT_API_KEY = '52c5fdebbbd836dc7ab7f82e22'
 import { useRouter } from 'next/router'
+import { useState } from 'react';
 import styles from '../../styles/Home.module.css'
 
 async function getPost(slug: string){
@@ -32,6 +33,28 @@ export const getStaticPaths=()=>{
     }
 }
 
+    
+    
+
+
+
+
+// <script>
+//     /**
+//     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+//     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+    
+  
+    
+//     (function() { // DON'T EDIT BELOW THIS LINE
+//     var d = document, s = d.createElement('script');
+//     s.src = 'https://mylog-1.disqus.com/embed.js';
+//     s.setAttribute('data-timestamp', +new Date());
+//     (d.head || d.body).appendChild(s);
+//     })();
+// </script>
+// <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
 
 type Post={
     title: string,
@@ -44,17 +67,41 @@ const Post: React.FC<{post: Post}>=(props)=>{
 
 
     const { post }=props
-console.log(post)
+    const [ enableLoadComments, setEnableLoadComments ]= useState<boolean>(true)
+// console.log(post)
     const router=useRouter()
 
     if(router.isFallback){
         return <h1>Loading...</h1>
     }
 
+function loadComments(){
+    //    setEnableLoadComments(false)    //!fix this issue
+    (window as any).disqus_config = function () {
+        this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = post.slug; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+
+
+        const script= document.createElement('script')
+        script.src= 'https://mylog-1.disqus.com/embed.js'
+        script.setAttribute('data-timestamp',Date.now().toString())
+
+        document.body.appendChild(script)
+
+    }
+
+
     return (<div className={styles.container}>
         <Link href='/'><a>Go back</a></Link>
         <h1>My blog Post</h1>
         <div dangerouslySetInnerHTML={{ __html:post.html}}></div>
+        {enableLoadComments && ( <p className={styles.goback} onClick={loadComments}>
+            Load Comments
+        </p>)}
+
+        <div id="disqus_thread"></div>
+        
         </div>
     )
 }
